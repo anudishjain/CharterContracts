a537dad53ec9c90e9c96a29a78e167d3803022f5
Anudish Jain <anudishj24@gmail.com>
2019-01-23 23:04:57 +0530

    Seperation Removed for Contracts

--- charterContract.sol
+++ charterContract.sol
@@ -1,3 +1,137 @@
+pragma solidity ^0.4.19;
+
+contract Owned {
+
+	address owner;
+
+	function Owned() public {
+
+		owner = msg.sender;
+	}
+
+	modifier onlyOwner() {
+
+		require(msg.sender == owner);
+		_;
+	}
+}
+
+contract Rent is Owned {
+
+	struct Person {
+
+		address eth;
+		string legalName;
+		string email;
+		uint aadhaar;
+
+		string sign;
+		
+		uint[] myOwned;
+		uint[] myRented;
+	}
+
+	struct Parties {
+
+		bool tenantApprove;
+		bool govApprove;
+
+		address landlord;
+		address tenant;
+		address government;
+
+		bool completed;
+
+		string sign_landlord;
+		string sign_tenant;
+	}
+
+	struct House {
+
+		string addressHouse;
+		string type_of_property;
+
+		uint duration;
+		uint rentAmount;
+
+		uint securityFee;
+		uint governFee; /// (also listed as registration fee)
+
+		bool completed;
+	}
+
+	struct OtherDetails {
+
+		string latitude;
+		string longitude;
+		string ipfs_url;
+
+		uint squareFootage;
+		uint numberBedrooms;
+
+		string others;
+		bool completed;
+	}
+	
+	struct Checks {
+		
+		bool isValid;
+		bool registerFee;
+		bool securityFee;
+
+		uint time_of_deploy;
+		uint end_date;
+	}
+	
+	uint private last_index;
+	
+	Parties[] public allParties; 
+	House[] public allHouses;
+	OtherDetails[] public allOtherDetails;
+	Checks[] public allChecks;
+
+	mapping(address => Person) public addressToPerson;
+	mapping(address => bool) private checkUser;
+	mapping(uint => bool) private checkAadhaar;
+	
+	mapping(address => uint) private landlordRegister;
+	mapping(address => uint) private tenantSecurity;
+
+
+	function Rent() public {
+
+		checkUser[owner] = true;
+		var govt = Person(owner, 'Government = Owner', 'Contact Government', 0, 'No Sign',  new uint[](0), new uint[](0));
+		
+		addressToPerson[owner] = govt;
+		
+		last_index = 0;
+	}
+
+	// --------------------------------------------------------------------------------------------------------------------------------
+
+	event startMessage(string message);
+
+	function createNewUser(string _name, string _email, uint _aadhaar, string _sign) external {
+
+		if((checkUser[msg.sender] == true)||(checkAadhaar[_aadhaar] == true))
+		{
+			startMessage('Failed !! User already registered..');
+		}
+
+		else if((checkUser[msg.sender] != true)&&(checkAadhaar[_aadhaar] != true))
+		{
+			var newUser = Person(msg.sender, _name, _email, _aadhaar, _sign, new uint[](0), new uint[](0));
+			addressToPerson[msg.sender] = newUser;
+
+			checkUser[msg.sender] = true;
+			checkAadhaar[_aadhaar] = true;
+
+			startMessage('Welcome !! Successfully registered..');
+		}
+	}
+
+	// --------------------------------------------------------------------------------------------------------------------------------
 
 	event registerParty(string message, uint status);
 
@@ -484,3 +618,91 @@
 	}
 	
 	// --------------------------------------------------------------------------------------------------------------------------------
+	
+	function govLogin() view external onlyOwner returns(string message, uint[] array, uint size) {
+		
+		var len = allParties.length;
+		uint[] memory indexes = new uint[](len);
+		
+		if(last_index + 1 <= allParties.length)
+		{
+			uint num = 0;
+			
+			for(uint i = last_index ; i < allParties.length ; i++)
+			{
+				var party = allParties[i];
+				var home = allHouses[i];
+				var detail = allOtherDetails[i];
+				var checks = allChecks[i];
+				
+				if((party.completed == true)&&(home.completed == true)&&(detail.completed == true)&&(checks.registerFee == true)&&
+					(checks.securityFee == true)&&(party.tenantApprove == true)&&(checks.time_of_deploy > 0)&&(party.govApprove == false))
+				{
+					indexes[num] = i;
+					num++;
+				}
+			}
+			
+			last_index = allParties.length;
+			return('Following Contracts have their Verification Pending', array, num);
+		}
+		
+		else
+		return('Done for Today.. No Pending Verifications', array, 0);
+		
+	}
+	
+	
+	function govApproval(uint i, uint _currentRate) external onlyOwner {
+		
+		require(i < allParties.length);
+		
+		var party = allParties[i];
+		var home = allHouses[i];
+		var detail = allOtherDetails[i];
+		var checks = allChecks[i];
+		
+		if((party.completed == true)&&(home.completed == true)&&(detail.completed == true)&&(checks.registerFee == true)&&
+			(checks.securityFee == true)&&(party.tenantApprove == true)&&(checks.time_of_deploy > 0))
+		{
+			party.govApprove = true; // government approves
+			checks.isValid = true; // marked as valid
+			
+			owner.transfer(home.governFee/_currentRate);
+			
+			checks.time_of_deploy = now;
+			checks.end_date = now + ((home.duration) * 4 weeks); /// months * 4 weeks
+		}
+	}
+	
+	function govReject(uint i, uint _currentRate) external onlyOwner {
+	    
+		require(i < allParties.length);
+		
+		var party = allParties[i];
+		var home = allHouses[i];
+		var detail = allOtherDetails[i];
+		var checks = allChecks[i];
+		
+		if((party.completed == true)&&(home.completed == true)&&(detail.completed == true)&&(checks.registerFee == true)&&
+			(checks.securityFee == true)&&(party.tenantApprove == true)&&(checks.time_of_deploy > 0))
+		{
+			party.govApprove = false; // government approves
+			checks.isValid = false; // marked as valid
+
+			var _tenant = party.tenant;
+			var _landlord = party.landlord;
+			
+			_landlord.transfer(home.governFee/_currentRate);
+			_tenant.transfer(home.securityFee/_currentRate); // refund the Registration Fee and Security Deposit back to parties
+
+			party.completed = false;
+			home.completed = false;
+			detail.completed = false;
+			
+			checks.time_of_deploy = 0;
+			checks.end_date = 0;
+		}	    
+	}
+
+}
